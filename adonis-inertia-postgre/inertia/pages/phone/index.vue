ec3c3f30ff20923253ff5343cb48e73a73f7284c
Clément Bilger <clement.bilger25@gmail.com>
2024-11-04 15:42:20 +0100

    DeleteModal & PhoneModal (create+edit)

--- adonis-inertia-postgre/inertia/pages/phone/index.vue
+++ adonis-inertia-postgre/inertia/pages/phone/index.vue
@@ -26,11 +26,36 @@ function deletePhone() {
     })
   }
 }
+
+const showPhoneModal = ref(false)
+const selectedPhone = ref(null)
+const mode = ref('create')
+
+function openCreateModal() {
+  selectedPhone.value = null
+  mode.value = 'create'
+  showPhoneModal.value = true
+}
+
+function openEditModal(phone) {
+  selectedPhone.value = phone
+  mode.value = 'edit'
+  showPhoneModal.value = true
+}
+
+function handleConfirm() {
+  showPhoneModal.value = false
+}
+
+function handleCancel() {
+  showPhoneModal.value = false
+}
 </script>
 
 <template>
   <div>
     <h1 class="text-2xl font-semibold">Liste des téléphones</h1>
+    <button @click="openCreateModal" class="text-blue-600 hover:underline">Ajouter un téléphone</button>
 
     <!-- Tableau ShadCN -->
     <Table>
@@ -50,25 +75,26 @@ function deletePhone() {
           <TableCell>{{ phone.number }}</TableCell>
           <TableCell>{{ phone.status }}</TableCell>
           <TableCell class="text-right">
-            <Link :href="`/phones/${phone.id}/edit`" class="text-blue-600 hover:underline"
-              >Modifier</Link
-            >
+            <button @click="openEditModal(phone)" class="text-blue-600 hover:underline">Modifier</button>
             <button @click="confirmDelete(phone.id)" class="text-red-600 hover:underline ml-2">Supprimer</button>
-
           </TableCell>
         </TableRow>
       </TableBody>
     </Table>
 
-    <div class="mt-4">
-      <Link href="/phones/create" class="text-blue-600 hover:underline">Ajouter un téléphone</Link>
-    </div>
-
-    <ConfirmationModal
+    <DeleteModal
       v-if="showModal"
       message="Voulez-vous vraiment supprimer ce téléphone ? Cette action est irréversible."
       @confirm="deletePhone"
       @cancel="showModal = false"
+    />
+
+    <PhoneModal
+      v-if="showPhoneModal"
+      :phone="selectedPhone"
+      :mode="mode"
+      @confirm="handleConfirm"
+      @cancel="handleCancel"
     />
   </div>
 </template>
